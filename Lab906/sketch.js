599c71907d4181d2da1dae8c1c8a059f52a71944
798150 <798150@seq.org>
2019-09-09 18:00:10 -0700

    Lab 906 finshed, needs testing.

---
+++ Lab906/sketch.js
@@ -0,0 +1,39 @@
+
+// Kyle Knudson
+//  9/05/19
+
+var ships = []; //declares array
+var mainBall;
+var mainBall2;
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  //background(5, 5, 5, 10);
+
+  loadObjects(300);
+
+
+}
+
+
+function draw() {
+background(5,5,5,20);
+
+  runObjects();
+}
+
+function loadObjects(x){
+  mainBall = new Ball(random(width/2), random(height/2), random (-1,1), random(-1,1), 1);
+  mainBall2 = new Ball(random(width/2), random(height/2), random (-1,1), random(-1,1), 1);
+  for(var i = 0; i < x; i++){
+    ships[i]=new Ship(random(width), random(height), random (-1,1), random(-1,1), i+3);
+  }
+}
+
+function runObjects(){
+  mainBall.run();
+  mainBall2.run();
+  for(var i = 0; i < balls.length; i++){
+    ships[i].run();
+}
+}
