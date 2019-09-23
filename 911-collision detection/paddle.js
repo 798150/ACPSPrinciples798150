73041a5bdd83150b6d630fce0cea08ae3814917b
798150 <798150@seq.org>
2019-09-23 08:50:47 -0700

    finishing 911

--- 911-collision detection/paddle.js
+++ 911-collision detection/paddle.js
@@ -1,20 +1,26 @@
 // Kyle Knudson
 // 9/11/19
 // This is a comment
-class Paddle{
-  constructor(x,y,w,h){
-    this.loc=createVector(x,y);
+var mouseLoc;
+class Paddle {
+  constructor(x, y, w, h){
+    this.clr = color(255, 0, 0);
+    this.loc=createVector(x, y);
     this.w=100;
     this.h=25;
-    this.clr= color(random(255), random(255), random(255));
   }
-  run(){
-    this.render();
-    this.checkEdges();
-    this.update();
+run(){
+  this.render();
+  this.update();
   }
-  render(){
-    fill(this.clr);
-    rect(this.loc.x, this.loc.y, this.w, this.h);
+
+render(){
+  fill(this.clr);
+  rect(this.loc.x, this.loc.y, this.w, this.h);
+  }
+
+update(){
+  var mouseLoc = createVector(mouseX, 700);
+  this.loc = p5.vector.lerp(this.loc, mouseLoc, 0.9)
   }
 }
--- 911-collision detection/sketch.js
+++ 911-collision detection/sketch.js
@@ -1,39 +1,29 @@
 
 // Kyle Knudson
 //  9/11/19
+// attraction
+// The setup function is called once when your program begins
 
-var ships = []; //declares array
-var mainBall;
-var mainBall2;
+var paddle;
+var ball;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   //background(5, 5, 5, 10);
-
-  loadObjects(300);
+  drawpaddle();
+  drawBall():
 
 
 }
-
 
 function draw() {
 background(5,5,5,20);
-
-  runObjects();
+  runPaddle();
+  runBall();
 }
 
-function loadObjects(x){
-  mainBall = new Ball(random(width/2), random(height/2), random (-1,1), random(-1,1), 1);
-  mainBall2 = new Ball(random(width/2), random(height/2), random (-1,1), random(-1,1), 1);
-  for(var i = 0; i < x; i++){
-    ships[i]=new Ship(random(width), random(height), random (-1,1), random(-1,1), i+3);
-  }
+function drawPaddle() {
+  paddle = new Paddle(100, 400, 300, 50);
 }
-
-function runObjects(){
-  mainBall.run();
-  mainBall2.run();
-  for(var i = 0; i < ships.length; i++){
-    ships[i].run();
 }
 }
