a1978ef8b0de46abd305095bd6b32aba0bfc0a4c
798150 <798150@seq.org>
2019-09-26 08:24:59 -0700

    Lab911

---
+++ Lab916-Paddle Game/sketch.js
@@ -0,0 +1,30 @@
+
+// Kyle Knudson
+//  9/11/19
+// Collision
+var balls = []
+var paddle;
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20, 20, 20);
+  loadObjects(3);
+}
+
+function draw() {
+  background(255,255,255, 60);
+  runObjects();
+  fill(2, 2, 2)
+}
+
+function loadObjects(n){
+  for(var i =0; i < n; i++){
+    balls[i] = new Ball(random(800), random(300), random(0, 5), random(0,5));
+  }
+    paddle = new Paddle(250, 700, 200, 25);
+}
+
+function runObjects(){
+  paddle.run();
+  for(var i = 0; i < balls.length; i++) balls[i].run();
+}
