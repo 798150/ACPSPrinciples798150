0be6cc1346a061e17c863abcbf2d6b7a4d14d408
798150 <798150@seq.org>
2019-12-11 08:51:40 -0800

    k

---
+++ 1211-Warm Up/sketch.js
@@ -0,0 +1,50 @@
+// Kyle Knudson
+// Warm up 1211
+var list=[];
+function setup(){
+  var cnv=createCanvas(800,800);
+  cnv.position((windowWidth-width)/2,30);
+  background(200);
+  loadNumbers();
+  mySort();
+  logNumber();
+  findAv();
+  findMedian();
+  }
+
+function loadNumbers(){
+  for (var i=0; i<11; i++){
+    list[i]= int(random(100));
+  }
+}
+
+function logNumber(){
+  console.log(list);
+}
+
+function findAv(){
+  sum=0;
+  for (var i=0; i<11; i++){
+    sum=(sum+list[i]);
+  }
+  console.log(sum/list.length);
+}
+
+function findMedian(){
+  console.log(list[(list.length-1)/2])
+}
+
+function swap(list,a,b){
+  var temp=list[a];
+  list[a]=list[b];
+  list[b]=temp;
+}
+function mySort(){
+  for(var i=0; i<list.length; i++){
+    for (var x=0; x<list.length;x++){
+      if(list[x]>list[x+1]){
+        swap(list,x,x+1);
+      }
+    }
+  }
+}
