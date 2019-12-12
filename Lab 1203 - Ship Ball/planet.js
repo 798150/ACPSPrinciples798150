394de2f790db11e353a26ba6a44467545d2f74dd
798150 <798150@seq.org>
2019-12-12 08:50:31 -0800

    re

--- Lab 1203 - Ship Ball/planet.js
+++ Lab 1203 - Ship Ball/planet.js
@@ -1,11 +1,11 @@
 // Kyle Knudson
 // 12/3/19
 // planet class
-class Ball{
+class Planet{
   constructor(x,y,dx,dy){
     this.loc= createVector(x,y);
     this.vel = createVector(dx,dy);
-    this.clr = color(0, 0, 255);
+    this.clr = color(random(255),random(255),random(255));
     this.acc=createVector(0,0);
     this.w=50;
 
@@ -17,6 +17,7 @@ class Ball{
     this.update();
   }
   checkEdges(){
+    //teleports planet if it hits any edges
     if(this.loc.x>800){
       this.loc.x=0;
     }
@@ -33,32 +34,14 @@ class Ball{
   }
 
   update(){
-    if(dist<100){
-      this.loc=createVector(random(width),random(height))
-    }
-    //var distToMainBallatt
-    //var distToMainBallrep
-//    if(this.id >= 0){
-  //    distToMainBallatt = this.loc.dist(attractor.loc);
-//      distToMainBallrep = this.loc.dist(repeller.loc);
-//      if (distToMainBallatt<500){
-//        //attraction
-//        this.acc= p5.Vector.sub(mainBallatt.loc,this.loc);
-//        this.acc.normalize();
-  //      this.acc.mult(.5);
-//      }
-//      if(distToMainBallrep<200){
-        //repulsion
-//        this.acc=p5.Vector.sub(this.loc,mainBallrep.loc);
-//        this.acc.normalize();
-//        this.acc.mult(0.5);
-//      }
-//    }
+    //if the ship gets close to planet
+    //planet moves
     this.vel.limit(3);
     this.loc.add(this.vel);
     this.vel.add(this.acc);
   }
   render(){
+    //renders planet
       fill(this.clr);
       ellipse(this.loc.x, this.loc.y, this.w, this.w);
     }
--- Lab 1203 - Ship Ball/ship.js
+++ Lab 1203 - Ship Ball/ship.js
@@ -1,13 +1,12 @@
 // Kyle Knudson
 // ship class
-var dist;
 class Ship{
   constructor(x,y,dx,dy){
     this.loc= createVector(x,y);
     this.vel = createVector(dx,dy);
     this.angle= 0;
     this.acc=createVector(0,0);
-    this.clr=color(255, 0, 0);
+    this.clr=color(random(255),random(255),random(255));
 
   }
 
@@ -17,6 +16,7 @@ class Ship{
     this.update();
   }
   checkEdges(){
+    //teleports ship if they hit edges
     if(this.loc.x<0 ) this.loc.x=width;
     if(this.loc.x>width) this.loc.x=0;
     if(this.loc.y<0 ) this.loc.y=height;
@@ -24,23 +24,30 @@ class Ship{
   }
 
   update(){
-    dist=this.loc.dist(planet.loc);
+    //ships move according to planet's position
     this.acc=p5.Vector.sub(planet.loc,this.loc)
     this.acc.normalize();
-    this.acc.mult(5);
+    this.acc.mult(sliderAttraction.value());
     this.vel.add(this.acc);
-    this.vel.limit(3);
+    this.vel.limit(sliderLimit.value());
     this.loc.add(this.vel);
+    if ((this.loc.dist(planet.loc))<20){
+      this.loc=createVector(random(800),random(800));
+      planet.loc=createVector(random(width),random(height));
+      planet.vel=createVector(random(-3,3),random(-3,3));
+    }
   }
 
   render(){
+    //renders ship
+    this.heading=this.vel.heading();
       fill(this.clr);
       push();
+      //rotates and translates ship
         translate(this.loc.x,this.loc.y);
-        rotate(this.loc.heading);
+        rotate(this.heading +1.5);
         triangle(-10,16,10,16,0,-16);
       pop();
     }
 
 }
-//end ball class
